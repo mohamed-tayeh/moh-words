e9b2f2d080b6c54913fd337d41af5fd89785bd2a
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-12-16 16:19:24 -0500

    Update CreateSubAnagramFile.java
    created createSubAnagramFile script to process all the words from words.json

--- src/main/java/com/mohamedtayeh/wosbot/scripts/CreateSubAnagramFile.java
+++ src/main/java/com/mohamedtayeh/wosbot/scripts/CreateSubAnagramFile.java
@@ -1,47 +1,53 @@
 package com.mohamedtayeh.wosbot.scripts;
 
-import com.mohamedtayeh.wosbot.features.anagramFile.AnagramFile;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mohamedtayeh.wosbot.features.constants.Constants;
 import com.mohamedtayeh.wosbot.features.subAnagramFile.SubAnagramFile;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class CreateSubAnagramFile implements Script {
-    private final AnagramFile anagramFile;
+    private final ObjectMapper objectMapper;
     private final SubAnagramFile subAnagramFile;
+    private final String wordPath;
 
-    public CreateSubAnagramFile(AnagramFile anagramFile, SubAnagramFile subAnagramFile) {
-        this.anagramFile = anagramFile;
+    public CreateSubAnagramFile(ObjectMapper objectMapper, SubAnagramFile subAnagramFile, String wordPath) {
+        this.objectMapper = objectMapper;
         this.subAnagramFile = subAnagramFile;
+        this.wordPath = wordPath;
     }
 
     @Override
     public void run() {
+        List<String> words;
 
-        // int size = anagramFile.getAnagrams().size();
-        int size = 5;
+        try {
+            words = objectMapper.readValue(new File(wordPath), new TypeReference<>() {
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
 
-        int counter = 0;
-        for (String anagram : anagramFile.getAnagrams().keySet()) {
-            if (counter > 4) {
-                break;
+        for (String word : words) {
+            if (word.length() < Constants.MIN_WORD_LENGTH || word.length() > Constants.MAX_WORD_LENGTH) {
+                continue;
             }
 
-            subAnagramFile.addWord(anagram);
-            counter++;
-            System.out.println(counter + "/" + size + " tasks submitted");
+            subAnagramFile.addWordFromFile(word);
         }
 
         ExecutorService executorService = subAnagramFile.getExecutorService();
+        executorService.shutdown();
         ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(1);
 
         scheduledExecutor.scheduleAtFixedRate(() -> {
-            long tasksCompleted = ((ThreadPoolExecutor) executorService).getCompletedTaskCount();
-            System.out.println(tasksCompleted + "/" + size + " tasks completed");
-            if (tasksCompleted == (long) size) {
+            System.out.println("Get Completed Tasks: " + ((ThreadPoolExecutor) executorService).getCompletedTaskCount());
+
+            if (executorService.isTerminated()) {
                 subAnagramFile.saveFile();
                 scheduledExecutor.shutdown();
                 executorService.shutdown();
