dbf0d8190a842f86af34509aab9c63ad105d98e3
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-12-16 16:19:34 -0500

    Update RunScript.java
    added methods for each script

--- src/main/java/com/mohamedtayeh/wosbot/scripts/RunScript.java
+++ src/main/java/com/mohamedtayeh/wosbot/scripts/RunScript.java
@@ -3,26 +3,38 @@ package com.mohamedtayeh.wosbot.scripts;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mohamedtayeh.wosbot.features.anagramFile.AnagramFile;
 import com.mohamedtayeh.wosbot.features.anagramHelper.AnagramHelper;
+import com.mohamedtayeh.wosbot.features.constants.FilePaths;
+import com.mohamedtayeh.wosbot.features.dictionaryApi.DictionaryApi;
 import com.mohamedtayeh.wosbot.features.subAnagramFile.SubAnagramFile;
 
 public class RunScript {
 
     public void run() {
+        createSubAnagramFile();
+    }
+
+    private void createSubAnagramFile() {
         ObjectMapper objectMapper = new ObjectMapper();
         AnagramHelper anagramHelper = new AnagramHelper();
         AnagramFile anagramFile = new AnagramFile(objectMapper, anagramHelper);
         SubAnagramFile subAnagramFile = new SubAnagramFile(objectMapper,
                 anagramHelper, anagramFile);
 
-        Script script = new CreateSubAnagramFile(anagramFile, subAnagramFile);
+        Script script = new CreateSubAnagramFile(objectMapper, subAnagramFile, FilePaths.WORDS_FILE);
         script.run();
-
-        // Script script = new CallDictionaryApi(new DictionaryApi(new ObjectMapper()));
-        // script.run();
-
-        // Script wordsToAnagram = new WordsToAnagram(new ObjectMapper(), new
-        // AnagramHelper(), FilePaths.WORDS_FILE,
-        // FilePaths.ANAGRAM_FILE);
-        // wordsToAnagram.run();
     }
+
+    private void callDictionaryApi() {
+        Script script = new CallDictionaryApi(new DictionaryApi(new ObjectMapper()));
+        script.run();
+    }
+
+
+    private void wordsToAnagram() {
+        Script wordsToAnagram = new WordsToAnagram(new ObjectMapper(), new
+                AnagramHelper(), FilePaths.WORDS_FILE,
+                FilePaths.ANAGRAM_FILE);
+        wordsToAnagram.run();
+    }
+
 }
