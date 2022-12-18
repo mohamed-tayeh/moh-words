f26a4770c90810d685a685972f2f359f58e69f7a
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-12-18 17:22:42 -0500

    Update addKeyToFile.py
    add scripts to change words.json to appropriate mongodb structure

--- src/scripts/words/addKeyToFile.py
+++ src/scripts/words/addKeyToFile.py
@@ -1,23 +1,66 @@
 import json
 
-with open('../../main/resources/prevSubAnagrams.json', 'r') as f:
-    with open('../../main/resources/subAnagramsV2.json', 'w') as f2:
-        print('reading file...')
+## Word file
+def wordFile(): 
+    with open('../../main/resources/words.json', 'r') as f:
+        with open('../../main/resources/wordsDict.json', 'w') as f2:
+            wordList = json.load(f)
+            count = 0
+            size = len(wordList)
+            wordDict = {}
 
-        subAnagrams = json.load(f)
+            for i, word in enumerate(wordList):
 
-        print('read file...')
+                wordList[i] = {'_id': word}
+                if count % 10000 == 0:
+                    print(f'Completed {count}/{size}')
 
-        count = 0
-        size = len(subAnagrams)
-        for key, v in subAnagrams.items():
-            subAnagrams[key] = {
-                'value': v,
-                '_id': key
-            }
+                count += 1
 
-            if count % 10000 == 0:
-                print(f'Completed {count}/{size}')
-            count += 1
+            json.dump(subAnagrams, f2)
 
-        json.dump(subAnagrams, f2)
+## Anagrams file
+def anagramFile():
+    with open('../../main/resources/anagrams.json', 'r') as f:
+        with open('../../main/resources/anagramsV2.json', 'w') as f2:
+            anagramDict = json.load(f)
+            count = 0
+            size = len(anagramDict)
+
+            for k, v in anagramDict.items():
+
+                anagramDict[k] = {
+                    '_id': k,
+                    'value': v
+                }
+
+                if count % 10000 == 0:
+                    print(f'Completed {count}/{size}')
+
+                count += 1
+
+            json.dump(anagramDict, f2)
+
+## SubAnagrams file
+def subAnagramFile():
+    with open('../../main/resources/prevSubAnagrams.json', 'r') as f:
+        with open('../../main/resources/subAnagramsV2.json', 'w') as f2:
+            subAnagrams = json.load(f)
+            count = 0
+            size = len(subAnagrams)
+            for key, v in subAnagrams.items():
+                subAnagrams[key] = {
+                    'value': v,
+                    '_id': key
+                }
+
+                if count % 10000 == 0:
+                    print(f'Completed {count}/{size}')
+                count += 1
+
+            json.dump(subAnagrams, f2)
+
+if __name__ == '__main__':
+    # wordFile()
+    anagramFile()
+    # subAnagramFile()
