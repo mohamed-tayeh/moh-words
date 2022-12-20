5300c802484f20a04c1899da7f32f2a89bb29747
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-12-20 12:01:35 -0500

    Update AnagramRes.java
    refactored to use string builder instead of stack

--- src/main/java/com/mohamedtayeh/wosbot/features/wordApi/responses/AnagramRes.java
+++ src/main/java/com/mohamedtayeh/wosbot/features/wordApi/responses/AnagramRes.java
@@ -4,60 +4,64 @@ package com.mohamedtayeh.wosbot.features.wordApi.responses;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
+import lombok.Data;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class AnagramRes {
-    private WordPage[] wordPages;
 
-    /**
-     * Returns a list of anagrams
-     *
-     * @return list of anagrams
-     */
-    public List<String> getAnagrams() {
+  private WordPage[] wordPages;
 
-        if (wordPages == null || wordPages.length == 0) {
-            return new ArrayList<>();
-        }
+  /**
+   * Returns a list of anagrams
+   *
+   * @return list of anagrams
+   */
+  public List<String> getAnagrams() {
 
-        ArrayList<String> anagrams = new ArrayList<>();
-        for (WordPage page : wordPages) {
-            for (PossibleWord word : page.getWordList()) {
-                anagrams.add(word.getWord());
-            }
-        }
-        return anagrams;
+    if (wordPages == null || wordPages.length == 0) {
+      return new ArrayList<>();
     }
 
-    /**
-     * Formats the anagrams by size with labels
-     *
-     * @return The string format of a list of anagrams
-     */
-    public String getAnagramsString() {
-
-        if (wordPages == null || wordPages.length == 0) {
-            return "";
-        }
-
-        Stack<String> anagrams = new Stack<>();
-        for (WordPage page : wordPages) {
-            anagrams.add("(" + page.getWordLength() + ")");
-            for (PossibleWord word : page.getWordList()) {
-                anagrams.add(word.getWord());
-            }
-            anagrams.add("|");
-        }
-
-        anagrams.pop();
-
-        return String.join(" ", anagrams);
+    ArrayList<String> anagrams = new ArrayList<>();
+    for (WordPage page : wordPages) {
+      for (PossibleWord word : page.getWordList()) {
+        anagrams.add(word.getWord());
+      }
     }
+    return anagrams;
+  }
+
+  /**
+   * Formats the anagrams by size with labels
+   *
+   * @return The string format of a list of anagrams
+   */
+  public String getAnagramsString() {
+
+    if (wordPages == null || wordPages.length == 0) {
+      return "";
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (WordPage page : wordPages) {
+      sb.append('(')
+          .append(page.getWordLength())
+          .append(')')
+          .append(' ');
+
+      for (PossibleWord word : page.getWordList()) {
+        sb.append(word.getWord()).append(' ');
+      }
+
+      sb.append("|").append(' ');
+    }
+
+    sb.deleteCharAt(sb.length() - 2);
+
+    return sb.toString();
+  }
 }
