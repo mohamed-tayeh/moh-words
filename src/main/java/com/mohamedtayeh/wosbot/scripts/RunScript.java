3fe3325f60f3ba13df7071f37a98a5cbafd3f1f2
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-12-21 11:59:34 -0500

    Update RunScript.java
    added a method for each script

--- src/main/java/com/mohamedtayeh/wosbot/scripts/RunScript.java
+++ src/main/java/com/mohamedtayeh/wosbot/scripts/RunScript.java
@@ -1,11 +1,37 @@
 package com.mohamedtayeh.wosbot.scripts;
 
-import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mohamedtayeh.wosbot.features.anagramFile.AnagramFile;
+import com.mohamedtayeh.wosbot.features.anagramHelper.AnagramHelper;
+import com.mohamedtayeh.wosbot.features.subAnagramFile.SubAnagramFile;
 
-@Service
 public class RunScript {
 
   public void run() {
+//    runWordsToAnagram();
+    runCreateSubAnagramFile();
   }
 
+  /**
+   * Runs the words to anagram script
+   */
+  public void runWordsToAnagram() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    AnagramHelper anagramHelper = new AnagramHelper();
+    WordsToAnagram wordsToAnagram = new WordsToAnagram(objectMapper, anagramHelper);
+    wordsToAnagram.run();
+  }
+
+  /**
+   * Runs the anagram to subAnagram script
+   */
+  public void runCreateSubAnagramFile() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    AnagramHelper anagramHelper = new AnagramHelper();
+    AnagramFile anagramFile = new AnagramFile(objectMapper, anagramHelper);
+    SubAnagramFile subAnagramFile = new SubAnagramFile(objectMapper, anagramHelper, anagramFile);
+    CreateSubAnagramFile createSubAnagramFile = new CreateSubAnagramFile(objectMapper,
+        subAnagramFile);
+    createSubAnagramFile.run();
+  }
 }
