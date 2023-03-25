ef3f4f78ee3b3b96e18510fc48f075516d53bc8e
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2023-03-25 11:16:45 -0400

    Update Bot.java
    use getBeansOfType instead of injecting each command into the bot

--- src/main/java/com/mohamedtayeh/wosbot/Bot.java
+++ src/main/java/com/mohamedtayeh/wosbot/Bot.java
@@ -1,114 +1,108 @@
 package com.mohamedtayeh.wosbot;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
 import com.github.philippheuer.events4j.simple.SimpleEventHandler;
 import com.github.twitch4j.ITwitchClient;
+import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.TwitchClientBuilder;
 import com.mohamedtayeh.wosbot.db.channel.ChannelController;
-import com.mohamedtayeh.wosbot.features.AddWordCommand;
-import com.mohamedtayeh.wosbot.features.DefineCommand;
-import com.mohamedtayeh.wosbot.features.GetAnagramsCommand;
-import com.mohamedtayeh.wosbot.features.GetWordsCommand;
-import com.mohamedtayeh.wosbot.features.JoinChannelCommand;
-import com.mohamedtayeh.wosbot.features.LeaveChannelCommand;
-import com.mohamedtayeh.wosbot.features.constants.FilePaths;
-import jakarta.annotation.PostConstruct;
+import com.mohamedtayeh.wosbot.features.commands.Command;
+import com.mohamedtayeh.wosbot.features.commands.CommandReflectingOnBot;
+import com.mohamedtayeh.wosbot.features.utils.FilePaths;
+import com.mohamedtayeh.wosbot.features.utils.GeneralUtils;
+import io.github.bucket4j.Bandwidth;
 import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Collection;
 import java.util.List;
-import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 @Service
-@NonNull
+@Slf4j
 public class Bot {
 
-  /**
-   * Twitch4J API
-   */
   private final ITwitchClient twitchClient;
+  private final Configuration configuration;
   private final ChannelController channelController;
-  private final GetWordsCommand getWordsCommand;
-  private final DefineCommand defineCommand;
-  private final GetAnagramsCommand getAnagramsCommand;
-  private final AddWordCommand addWordCommand;
-  private final JoinChannelCommand joinChannelCommand;
-  private final LeaveChannelCommand leaveChannelCommand;
+  private final ApplicationContext applicationContext;
+
+  public Bot(ChannelController channelController, ApplicationContext applicationContext) {
+    this.configuration = loadConfiguration();
+    this.twitchClient = createTwitchClient();
+    this.channelController = channelController;
+    this.applicationContext = applicationContext;
+    this.registerFeatures();
+  }
 
   /**
-   * Holds the Bot Configuration
+   * Method to register all features
    */
-  private Configuration configuration;
+  public void registerFeatures() {
+    // Register Event Handlers
+    SimpleEventHandler eventHandler = twitchClient.getEventManager()
+        .getEventHandler(SimpleEventHandler.class);
 
-  public Bot(ChannelController channelController, DefineCommand defineCommand,
-      GetAnagramsCommand getAnagramsCommand,
-      GetWordsCommand getWordsCommand, AddWordCommand addWordCommand,
-      JoinChannelCommand joinChannelCommand, LeaveChannelCommand leaveChannelCommand) {
-    // Load Configuration
-    loadConfiguration();
+    log.info("I am about to get the commands!");
+    Collection<Command> commands = applicationContext.getBeansOfType(Command.class).values();
+    log.info("{} commands are ready", commands.size());
 
+    for (Command command : commands) {
+
+      log.info("getting command: {}", command);
+
+      if (command instanceof CommandReflectingOnBot commandReflectingOnBot) {
+        commandReflectingOnBot.setBot(this);
+      }
+
+      command.handleEvent(eventHandler);
+    }
+  }
+
+  private TwitchClient createTwitchClient() {
     TwitchClientBuilder clientBuilder = TwitchClientBuilder.builder();
 
     OAuth2Credential credential = new OAuth2Credential(
         "twitch",
         configuration.getCredentials().get("irc"));
 
-    twitchClient = clientBuilder
+    return clientBuilder
         .withClientId(configuration.getApi().get("twitch_client_id"))
         .withClientSecret(configuration.getApi().get("twitch_client_secret"))
         .withEnableHelix(true)
         .withChatAccount(credential)
         .withEnableChat(true)
+        .withChatChannelMessageLimit(
+            Bandwidth.simple(1, Duration.ofSeconds(2)).withId("per-channel-limit"))
         .build();
-//        .withChatChannelMessageLimit(
-//            Bandwidth.simple(1, Duration.ofSeconds(2)).withId("per-channel-limit"))
-
-    this.channelController = channelController;
-    this.defineCommand = defineCommand;
-    this.getAnagramsCommand = getAnagramsCommand;
-    this.getWordsCommand = getWordsCommand;
-    this.addWordCommand = addWordCommand;
-    this.joinChannelCommand = joinChannelCommand;
-    this.joinChannelCommand.setBot(this);
-    this.leaveChannelCommand = leaveChannelCommand;
-    this.leaveChannelCommand.setBot(this);
-  }
-
-  /**
-   * Method to register all features
-   */
-  @PostConstruct
-  public void registerFeatures() {
-    // Register Event Handlers
-    SimpleEventHandler eventHandler = twitchClient.getEventManager()
-        .getEventHandler(SimpleEventHandler.class);
-    defineCommand.handleEvent(eventHandler);
-    getAnagramsCommand.handleEvent(eventHandler);
-    getWordsCommand.handleEvent(eventHandler);
-    addWordCommand.handleEvent(eventHandler);
-    joinChannelCommand.handleEvent(eventHandler);
-    leaveChannelCommand.handleEvent(eventHandler);
   }
 
   /**
    * Load the Configuration
    */
-  private void loadConfiguration() {
+  private Configuration loadConfiguration() {
     try {
       // Credentials
-      ObjectMapper mapper = new ObjectMapper();
-      configuration = mapper.readValue(new File(FilePaths.CONFIG_FILE_NAME), Configuration.class);
+      Configuration configuration = GeneralUtils.objectMapper.readValue(
+          new File(FilePaths.CONFIG_FILE_NAME),
+          Configuration.class);
+
       // Channels
-      List<String> channels = mapper.readValue(new File(FilePaths.CHANNELS_FILE_NAME),
+      List<String> channels = GeneralUtils.objectMapper.readValue(
+          new File(FilePaths.CHANNELS_FILE_NAME),
           new TypeReference<>() {
           });
+
       configuration.setChannels(channels);
 
-    } catch (Exception ex) {
-      ex.printStackTrace();
-      System.out.println("Unable to load Configuration ... Exiting.");
+      return configuration;
+    } catch (IOException e) {
+      log.error("Unable to load Configuration ... Exiting.", e);
       System.exit(1);
+      throw new RuntimeException(e);
     }
   }
 
