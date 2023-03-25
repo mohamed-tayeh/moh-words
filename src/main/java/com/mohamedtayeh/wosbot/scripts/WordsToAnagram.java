e10538602aa4968e6f26f88be9c20f21fe7a60ff
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2023-03-25 11:17:10 -0400

    Update WordsToAnagram.java
    use static object mapper

--- src/main/java/com/mohamedtayeh/wosbot/scripts/WordsToAnagram.java
+++ src/main/java/com/mohamedtayeh/wosbot/scripts/WordsToAnagram.java
@@ -1,38 +1,37 @@
 package com.mohamedtayeh.wosbot.scripts;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mohamedtayeh.wosbot.features.anagramHelper.AnagramHelper;
-import com.mohamedtayeh.wosbot.features.constants.Constants;
-import com.mohamedtayeh.wosbot.features.constants.FilePaths;
+import com.mohamedtayeh.wosbot.features.utils.Constants;
+import com.mohamedtayeh.wosbot.features.utils.FilePaths;
+import com.mohamedtayeh.wosbot.features.utils.GeneralUtils;
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.TreeSet;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
-public class WordsToAnagram {
+@Slf4j
+public class WordsToAnagram implements Script {
 
-  private final ObjectMapper objectMapper;
   private final AnagramHelper anagramHelper;
-
-  public void run() {
-    wordListToAnagramStruct();
-  }
 
   /**
    * Takes a filePath to a JSON for a word list and converts it
    */
-  public void wordListToAnagramStruct() {
+  public Integer call() {
     List<String> words;
 
     try {
-      words = objectMapper.readValue(new File(FilePaths.WORDS_FILE), new TypeReference<>() {
-      });
+      words = GeneralUtils.objectMapper.readValue(new File(FilePaths.WORDS_FILE),
+          new TypeReference<>() {
+          });
     } catch (Exception e) {
       e.printStackTrace();
-      return;
+      return 1;
     }
 
     HashMap<String, TreeSet<String>> anagrams = new HashMap<>();
@@ -54,9 +53,11 @@ public class WordsToAnagram {
     }
 
     try {
-      objectMapper.writeValue(new File(FilePaths.ANAGRAM_FILE), anagrams);
-    } catch (Exception e) {
-      e.printStackTrace();
+      GeneralUtils.objectMapper.writeValue(new File(FilePaths.ANAGRAM_FILE), anagrams);
+      return 0;
+    } catch (IOException e) {
+      log.error("Couldn't save anagram file", e);
+      return 1;
     }
   }
 }
