d0006c02d76ada28d32d9d487d8e95c92a0e7a00
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2023-03-25 11:15:09 -0400

    Create InvalidUserInput.java

---
+++ src/main/java/com/mohamedtayeh/wosbot/features/commands/getWordsCommand/Exceptions/InvalidUserInput.java
@@ -0,0 +1,9 @@
+package com.mohamedtayeh.wosbot.features.commands.getWordsCommand.Exceptions;
+
+public class InvalidUserInput extends Exception {
+
+  public InvalidUserInput(String message) {
+    super(message);
+  }
+}
+
