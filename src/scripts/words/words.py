330d1cbcad34f7dbeeb8842b8bb8896fc912b234
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2023-03-25 11:15:24 -0400

    Update words.py
    add doc string

--- src/scripts/words/words.py
+++ src/scripts/words/words.py
@@ -3,6 +3,7 @@ from nltk.corpus import words
 import wn
 
 def load_words(option: str, asSet = False) -> set:
+
     if option == "english-words":
         with open('words_alpha.txt') as word_file:
             if asSet:
@@ -10,23 +11,29 @@ def load_words(option: str, asSet = False) -> set:
             else:
                 valid_words = word_file.read().split()
         return valid_words
-    elif option == "wordnet":
+    
+    if option == "wordnet":
         en = wn.Wordnet('oewn:2021')
         if asSet:
             wnWords = {word.id[5:len(word.id) - 2] for word in en.words()}
         else:
             wnWords = [word.id[5:len(word.id) - 2] for word in en.words()]
         return wnWords
-    elif option == "nltk":
+    
+    if option == "nltk":
         if asSet:
             nltkWords = set(words.words())
         else:
             nltkWords = words.words()
         return nltkWords
-    else:
-        raise ValueError("Invalid option")
+    
+    raise ValueError("Invalid option")
 
 if __name__ == "__main__":
+    """
+    Creates a list of words and dumps them into a json file based on the selected
+    source.
+    """
     word_list = load_words("english-words")
     with open('../../main/resources/words.json', 'w') as f:
         json.dump(word_list, f)
